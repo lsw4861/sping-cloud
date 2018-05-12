c4bf1024535bcdc3ddca2a9a293b638defe60551
lsw4861 <294880292@qq.com>
2018-05-13 00:22:02 +0800

    first commit

---
+++ config/src/main/java/cn/liang/config/ConfigApplication.java
@@ -0,0 +1,22 @@
+package cn.liang.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Auther: pc
+ * @Date: 2018/5/12 23:43
+ * @Description:
+ */
+@EnableDiscoveryClient
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
---
+++ registry/src/main/java/cn/liang/registry/RegistryApplication.java
@@ -0,0 +1,18 @@
+package cn.liang.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Auther: pc
+ * @Date: 2018/5/12 23:05
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+}
